065e82160da96df1af0bbcbbb3079b9a3a8a2109
Deonarine <adeonari@r1-core.r1.aig.net>
2015-11-16 15:51:51 -0500

    Initial

---
+++ Winton_2.R
@@ -0,0 +1,48 @@
+library(readr)
+library(dplyr)
+library(tidyr)
+library(rstan)
+
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores(),
+        stringsAsFactors = FALSE,
+        scipen = 10) 
+
+#' coalesce function
+`%||%` <- function(a, b) ifelse(!is.na(a), a, b)
+
+train.full <- read_csv("data/train.csv.zip")
+test.full <- read_csv("data/test.csv.zip")
+sample_submission <- read_csv("data/sample_submission.csv.zip")
+
+names(train.full)
+
+features <- train.full %>%
+  select(matches("Feature"))
+
+f.cor <- cor(features, use="pairwise.complete.obs")
+
+f.cor <- apply(f.cor, 2, function (x) ifelse(x==1,0,x))
+f.cor.max <- apply(f.cor, 1, max)
+f.cor.max
+
+# impute missing features
+train.imp <- train.full %>%
+  mutate(Feature_3 = Feature_3 %||% Feature_4,
+         Feature_4 = Feature_4 %||% Feature_3,
+         Feature_6 = Feature_6 %||% Feature_21,
+         Feature_21 = Feature_21 %||% Feature_6,
+         Feature_18 = Feature_18 %||% Feature_21,
+         Feature_21 = Feature_21 %||% Feature_18)
+
+
+train_y2 <- train.full %>%
+  replace_na(list(Feature_1=0,Feature_2=0,Feature_3=0,Feature_4=0,Feature_5=0,Feature_6=0,Feature_7=0,Feature_8=0,Feature_9=0,Feature_10=0,
+                  Feature_11=0,Feature_12=0,Feature_13=0,Feature_14=0,Feature_15=0,Feature_16=0,Feature_17=0,Feature_18=0,Feature_19=0,Feature_20=0,
+                  Feature_21=0,Feature_22=0,Feature_23=0,Feature_24=0,Feature_25=0)) %>%
+  #filter(!is.na(Feature_2), !is.na(Feature_3)) %>%
+  sample_n(5000)
+
+train.imp %>%
+  mutate(na.var = ifelse(is.na(Feature_3), 1, 0)) %>%
+  count(na.var)

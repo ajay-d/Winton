4207654cf0cb1de8da8d961c614f6781dccdd8ba
Deonarine <adeonari@r1-core.r1.aig.net>
2015-12-31 17:25:42 -0500

    score

--- Winton_intra_only.R
+++ Winton_intra_only.R
@@ -268,9 +268,9 @@ dat <- list('N' = dim(train.sample)[[1]], #number of obs
             'weights' = train.sample$Weight_Intraday
             )
 
-fit <- stan('stan_intra_0beta_simplex.stan',
+fit <- stan('stan_intra_0beta_vector.stan',
             model_name = "Stan_intra", 
-            iter=1000, warmup=500,
+            iter=500, warmup=250,
             thin=2, chains=3, seed=252014,
             data = dat)
 
@@ -292,4 +292,4 @@ plot(fit, pars=c("sigma_1", 'sigma_2'))
 plot(fit, pars=c("alpha"))
 plot(fit, pars=c("beta"))
 
-save(fit, file='Stan_intra_0beta_3Q.RData')
+save(fit, file='Stan_intra_0beta_3Q_500.RData')
--- Winton_score.R
+++ Winton_score.R
@@ -67,7 +67,10 @@ test.imp <- cbind(test.full, df) %>%
          # -+-
          level_8 = ifelse(sign(Ret_MinusOne) != sign(Ret_MinusTwo) & 
                             sign(Ret_MinusOne) != sign(return.intra) & 
-                            -1 == sign(return.intra), 8, level_8))
+                            -1 == sign(return.intra), 8, level_8)) %>%
+  replace_na(list(Feature_1=0,Feature_2=0,Feature_3=0,Feature_4=0,Feature_5=0,Feature_6=0,Feature_7=0,Feature_8=0,Feature_9=0,Feature_10=0,
+                  Feature_11=0,Feature_12=0,Feature_13=0,Feature_14=0,Feature_15=0,Feature_16=0,Feature_17=0,Feature_18=0,Feature_19=0,Feature_20=0,
+                  Feature_21=0,Feature_22=0,Feature_23=0,Feature_24=0,Feature_25=0))
 
 load("Stan_inter_0beta.RData")
 inter_0beta <- extract(fit, pars=c("alpha", "theta"), permuted = TRUE)
@@ -82,6 +85,7 @@ median(inter_0beta$alpha[,7,2])
 
 theta <- apply(inter_0beta$theta, 2:4, median)
 
+#####0 Beta ####
 #inter day returns (0 beta):
 y.pred.intra <- NULL
 for(d in sort(unique(test.imp$level_8))) {
@@ -109,3 +113,40 @@ y.pred.intra <- y.pred.intra %>%
   select(Id=Id.merge, Predicted) %>%
   arrange(Id)
 
+
+load("Stan_inter_1beta.RData")
+inter_1beta <- extract(fit, pars=c("alpha", "theta", "beta"), permuted = TRUE)
+
+alpha <- apply(inter_1beta$alpha, 2:3, median)
+theta <- apply(inter_1beta$theta, 2:4, median)
+beta <- apply(inter_1beta$beta, 2:4, median)
+
+#####1 Beta ####
+#inter day returns (1 beta):
+y.pred.intra <- NULL
+for(d in sort(unique(test.imp$level_8))) {
+  
+  t.d <- filter(test.imp, level_8==d)
+  x.d <- t.d %>% select(Ret_MinusTwo, Ret_MinusOne, return.intra)
+  covar.d <- t.d %>% select(matches("Feature"))
+  
+  theta.d <- theta[d,,]
+  alpha.d <- alpha[d,]
+  #beta.d <- beta[d,,]
+  
+  y.pred.d <- as.matrix(x.d) %*% as.matrix(t(theta.d)) + alpha.d
+  
+  y.pred.d <- t.d %>% 
+    select(Id) %>%
+    bind_cols(as.data.frame(y.pred.d)) %>%
+    setNames(c('Id','61','62'))
+  
+  y.pred.intra <- bind_rows(y.pred.intra, y.pred.d)
+    
+}
+
+y.pred.intra <- y.pred.intra %>%
+  gather(Id.y, Predicted, -Id) %>%
+  mutate(Id.merge = paste0(Id, "_", Id.y)) %>%
+  select(Id=Id.merge, Predicted) %>%
+  arrange(Id)
